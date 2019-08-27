32d24d670ac860c61f7c1606bdea644ea788cf70
KostyaAdnrykhin <home222.home@mail.ru>
2019-08-27 18:56:30 +0700

    Добавьте файлы проекта.

---
+++ ForstCorporation@2019/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ForstCorporation_2019
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        [System.Obsolete]
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.metroTabControl = new MetroFramework.Controls.MetroTabPage();
+            this.metroCheckBox1 = new MetroFramework.Controls.MetroCheckBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTabControl1.SuspendLayout();
+            this.metroTabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Controls.Add(this.metroTabControl);
+            this.metroTabControl1.Location = new System.Drawing.Point(331, 404);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Size = new System.Drawing.Size(309, 151);
+            this.metroTabControl1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroTabControl1.TabIndex = 0;
+            this.metroTabControl1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // metroTabControl
+            // 
+            this.metroTabControl.Controls.Add(this.metroCheckBox1);
+            this.metroTabControl.Controls.Add(this.metroButton1);
+            this.metroTabControl.Controls.Add(this.metroTextBox2);
+            this.metroTabControl.Controls.Add(this.metroTextBox1);
+            this.metroTabControl.Cursor = System.Windows.Forms.Cursors.Default;
+            this.metroTabControl.HorizontalScrollbarBarColor = true;
+            this.metroTabControl.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabControl.HorizontalScrollbarSize = 10;
+            this.metroTabControl.Location = new System.Drawing.Point(4, 38);
+            this.metroTabControl.Name = "metroTabControl";
+            this.metroTabControl.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.metroTabControl.Size = new System.Drawing.Size(301, 109);
+            this.metroTabControl.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroTabControl.TabIndex = 0;
+            this.metroTabControl.Text = "                                Login                                ";
+            this.metroTabControl.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTabControl.VerticalScrollbarBarColor = true;
+            this.metroTabControl.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabControl.VerticalScrollbarSize = 10;
+            // 
+            // metroCheckBox1
+            // 
+            this.metroCheckBox1.AutoSize = true;
+            this.metroCheckBox1.Location = new System.Drawing.Point(-62, 220);
+            this.metroCheckBox1.Name = "metroCheckBox1";
+            this.metroCheckBox1.Size = new System.Drawing.Size(16, 0);
+            this.metroCheckBox1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroCheckBox1.TabIndex = 3;
+            this.metroCheckBox1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroCheckBox1.UseSelectable = true;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(0, 66);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(301, 23);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "Login";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroTextBox2
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox2.CustomButton.Image = null;
+            this.metroTextBox2.CustomButton.Location = new System.Drawing.Point(158, 1);
+            this.metroTextBox2.CustomButton.Name = "";
+            this.metroTextBox2.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox2.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox2.CustomButton.TabIndex = 1;
+            this.metroTextBox2.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox2.CustomButton.UseSelectable = true;
+            this.metroTextBox2.CustomButton.Visible = false;
+            this.metroTextBox2.Lines = new string[0];
+            this.metroTextBox2.Location = new System.Drawing.Point(58, 37);
+            this.metroTextBox2.MaxLength = 32767;
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.PasswordChar = '●';
+            this.metroTextBox2.PromptText = "Password";
+            this.metroTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox2.SelectedText = "";
+            this.metroTextBox2.SelectionLength = 0;
+            this.metroTextBox2.SelectionStart = 0;
+            this.metroTextBox2.ShortcutsEnabled = true;
+            this.metroTextBox2.Size = new System.Drawing.Size(180, 23);
+            this.metroTextBox2.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroTextBox2.TabIndex = 5;
+            this.metroTextBox2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTextBox2.UseSelectable = true;
+            this.metroTextBox2.WaterMark = "Password";
+            this.metroTextBox2.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox2.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(158, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(58, 8);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.PromptText = "Login";
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(180, 23);
+            this.metroTextBox1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroTextBox1.TabIndex = 4;
+            this.metroTextBox1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMark = "Login";
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ForstCorporation_2019.Properties.Resources.DOTA_2_Lycan_Ursa_443405_2048x1152;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(950, 641);
+            this.Controls.Add(this.metroTabControl1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(950, 641);
+            this.MinimumSize = new System.Drawing.Size(950, 641);
+            this.Name = "Form1";
+            this.Text = "Forst Corporation";
+            this.metroTabControl1.ResumeLayout(false);
+            this.metroTabControl.ResumeLayout(false);
+            this.metroTabControl.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTabPage metroTabControl;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox1;
+    }
+}
+
---
+++ ForstCorporation@2019/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using System.Diagnostics;
+
+/*
+ * Size: 355, 237
+ * Style: Red
+ * Theme: Light
+ */
+
+namespace ForstCorporation_2019
+{
+    public partial class Form1 : Form
+    {
+        bool login;
+        bool usergroup;
+
+        [Obsolete]
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            metroCheckBox1.Checked = true;
+
+            var steam = "steam";
+            var starget = Process.GetProcessesByName(steam).FirstOrDefault();
+
+            metroTextBox1.Text = Properties.Settings.Default.Login;
+            metroTextBox2.Text = Properties.Settings.Default.Password;
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            login = true;
+
+            Properties.Settings.Default.Login = metroTextBox1.Text;
+            Properties.Settings.Default.Password = metroTextBox2.Text;
+            Properties.Settings.Default.Cheackbox = metroCheckBox1.Checked;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
+
+        // 355, 218
+
+        
